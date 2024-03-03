4edd91ffe5c62b0cfbad556c423022c008c6b6e7
Giovanni Fulco <giovanni.fulco@gmail.com>
2024-03-03 18:46:01 +0100

    Add version to common.sh (#139)

--- bin/common.sh
+++ bin/common.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Tidal Connect - https://github.com/GioF71/tidal-connect.git - common.sh version 0.1.0"
+
 # some constants
 ASOUND_CONF_SIMPLE_FILE=asound.conf
 ASOUND_CONF_FILE=/etc/$ASOUND_CONF_SIMPLE_FILE
